8024685f01f65936555eae86d6aaf91c7782f984
SeoYoonLim <seoyoonlim.21@gmail.com>
2025-07-02 17:21:10 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int money) {
+        int cups = money /5500;
+        int change = money %5500;
+        int[] answer = {cups, change};
+        return answer;
+    }
+}
